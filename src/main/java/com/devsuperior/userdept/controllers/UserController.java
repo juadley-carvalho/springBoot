c5453d920b4b0af99078f038839933a2e9439965
juadley <juadley.carvalho@hotmail.com>
2022-10-31 13:17:05 -0300

    Criado endpoint para busca por nome de usuário

--- src/main/java/com/devsuperior/userdept/controllers/UserController.java
+++ src/main/java/com/devsuperior/userdept/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.userdept.entities.User;
@@ -30,6 +31,11 @@ public class UserController {
     public User findById(@PathVariable Long id){
         User result = repository.findById(id).get();
         return result;
+    }
+
+    @GetMapping(value = "/name")
+    public List<User> findByName(@RequestParam String name){
+        return repository.findByName(name);
     }
 
     @PostMapping
--- src/main/java/com/devsuperior/userdept/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/userdept/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.devsuperior.userdept.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.userdept.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+
+    List<User> findByName(String name);
     
 }
